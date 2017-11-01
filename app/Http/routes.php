888c9b226306da4149a93c23eb8b1878e553fce0
tony <tony@tony.local>
2017-11-01 10:34:44 +0800

    練習6-2

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -21,10 +21,10 @@ Route::get('1',function(){
     #]);
     #$post=\App\Post::find(1);
     #$post->delete();
-    $allPosts=\App\Post::all();
-    dd($allPosts);
-    $featuredPosts=\App\Post::where('is_feature',1)‐>get();
-    dd($featuredPosts);
+    $fourthPost=\App\Post::find(4);
+    dd($fourthPost);
+    $lastPost=\App\Post::orderBy('id','DESC')‐>first();
+    dd($lastPost);
     #$post->title=' saved title';
     #$post->content='saved content';
     #$post->save();
